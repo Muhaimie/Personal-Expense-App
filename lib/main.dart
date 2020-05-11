0e1c61f75c0635b70c8e784b880408c483da573e
Muhaimie Mazlah <laws_basic@yahoo.co.uk>
2020-05-11 13:21:56 +0800

    ChartBar

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+
+import 'package:flutter/material.dart';
+
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+
+
+
+//import './models/transaction.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+        accentColor: Colors.lightGreen,
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title:TextStyle(
+            fontFamily:"",
+            fontSize:30
+          )
+        ),
+        // fontFamily: "",
+        // appBarTheme: AppBarTheme(
+        //   textTheme: ThemeData.light().textTheme.copyWith(
+        //     title: TextStyle(
+        //       fontFamily:"",
+        //       fontSize:30
+        //     )
+        //   )
+        // ),
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _userTransaction = [
+    // Transaction(id: "t1", title: "New shoes", amount: 69.99, date: DateTime.now()),
+    // Transaction(id: "t2", title: "Grocery", amount: 22.99, date: DateTime.now())
+  
+  ];
+
+  List<Transaction> get _recentTransaction{
+
+    return _userTransaction.where((element) {
+        return element.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+
+    } 
+    ).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount){
+    final newTx = Transaction(title: txTitle, amount: txAmount,id: DateTime.now().toString(),date: DateTime.now());
+    
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+
+  void _startAddNewTransaction(BuildContext context){
+    showModalBottomSheet(context: context, builder: (_){
+        return GestureDetector(
+          child:NewTransaction(_addNewTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        ) ;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Personal Expenses"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.add), onPressed: (){
+              _startAddNewTransaction(context);
+            })
+          ],
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Chart(_recentTransaction),
+              TransactionList(_userTransaction)
+              
+            ]
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: FloatingActionButton(child:Icon(Icons.add) ,onPressed: ()=> _startAddNewTransaction(context)),
+        )
+
+    ;
+  }
+}
+
